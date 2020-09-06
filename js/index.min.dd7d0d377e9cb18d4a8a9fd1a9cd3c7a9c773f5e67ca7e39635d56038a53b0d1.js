e2564414efe4720f7c81d89debbf1f8fc2b02420
Alexandru G Buzneanu <bznnalex@gmail.com>
2020-09-06 15:45:29 +0100

    Add public blog resources

---
+++ js/index.min.dd7d0d377e9cb18d4a8a9fd1a9cd3c7a9c773f5e67ca7e39635d56038a53b0d1.js
@@ -0,0 +1,3 @@
+function toggleMobileNavState(){const body=document.querySelector("body");body.classList.toggle("nav--active");}
+function initBurger(){const burger=document.querySelector(".burger");burger.addEventListener("click",toggleMobileNavState);}
+initBurger();document.addEventListener('DOMContentLoaded',(event)=>{((localStorage.getItem('mode')||'dark')==='dark')?document.querySelectorAll('.switchme').forEach(function(item){item.classList.add('dark');}):document.querySelectorAll('.switchme').forEach(function(item){item.classList.remove('dark');})});
